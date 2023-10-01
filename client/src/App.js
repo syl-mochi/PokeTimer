7654e9c67ed3bae8027e38134c5f65d7f9d21b01
Shi Yu Liu <22544476+syl-mochi@users.noreply.github.com>
2023-09-30 22:33:50 -0400

    pokeapi call, client to server call

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,29 @@
 import styles from "./App.module.css";
-
 import Countdown from "./components/Countdown/Countdown";
+import axios from "axios";
+
+const getPokemonByID = "http://localhost:5000/getPokemon?id=";
 
 function App() {
     return (
         <>
             <div className={styles.example}>Pretend there is Squirtle</div>
             <Countdown />
+
+            <button type="button" onClick={getCompanionJSON(179)}>
+                mareep
+            </button>
         </>
     );
+}
+
+function getCompanionJSON(id) {
+    var companionJSON = async () =>
+        await axios
+            .get(getPokemonByID.concat(id))
+            .then((res) => console.log(res))
+            .catch((err) => console.log(err.message));
+    return companionJSON;
 }
 
 export default App;
--- server/app.js
+++ server/app.js
@@ -1,9 +1,42 @@
 import express from "express";
+import * as api from "./api.js";
+
+// need cors to allow IP to pass
+import cors from "cors";
 
 const app = express();
+
+// note that server runs on LH:5000
+
+// https://expressjs.com/en/resources/middleware/cors.html
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("dis is where it starts :v");
 });
 
+/** Give the pokemon data(id, name, sprite) of the associated given id in JSON*/
+app.get("/getPokemon", async (req, res) => {
+    var companion = await api.getPokemon(req.query.id);
+    var companionJSON = JSON.stringify(companion);
+    res.send(companionJSON);
+});
+
+app.get("/getPokemonName", (req, res) => {
+    res.send("plain-text string data from JSON");
+});
+
+app.get("/getPokemonPNG", (req, res) => {
+    res.send("plain-text string data from JSON");
+});
+
 export default app;
+
+const main = async () => {
+    // let mareep = await api.getPokemon("mareep");
+    // console.log(mareep["name"]);
+    // console.log(mareep["id"]);
+    // console.log(mareep["sprite"]);
+};
+
+main();
